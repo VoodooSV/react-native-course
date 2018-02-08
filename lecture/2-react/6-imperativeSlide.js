03b3a7c262bc328e3e34bdb2aced82a21f02e38b
VoodooSV <voodoosv@gmail.com>
2018-02-08 13:44:17 +0200

    Add function invocation

--- lecture/2-react/6-imperativeSlide.js
+++ lecture/2-react/6-imperativeSlide.js
@@ -1,30 +1,36 @@
+function createElement(el) {
+
+}
+
 const SLIDE = {
-  title: 'React is Declarative',
-  bullets: [
-    'Imeritive vs Declaraive',
-    "The browser APIs are't fun to work with",
-    'React allows us to write what we want, and the library will take care of the DOM manipulation',
-  ],
+    title: 'React is Declarative',
+    bullets: [
+        'Imeritive vs Declaraive',
+        "The browser APIs are't fun to work with",
+        'React allows us to write what we want, and the library will take care of the DOM manipulation',
+    ],
 }
 
 const CLASSNAMES = {title: 'title', bullet: 'bullet'}
 
 function createSlide(slide) {
-  const slideElement = document.createElement('div')
+    const slideElement = document.createElement('div')
 
-  const title = document.createElement('h1')
-  title.className = CLASSNAMES.title
-  title.innerHTML = slide.title
-  slideElement.appendChild(title)
+    const title = document.createElement('h1')
+    title.className = CLASSNAMES.title
+    title.innerHTML = slide.title
+    slideElement.appendChild(title)
 
-  const bullets = document.createElement('ul')
-  slide.bullets.forEach(bullet => {
-    const bulletElement = document.createElement('li')
-    bulletElement.className = CLASSNAMES.bullet
-    bulletElement.innerHTML = bullet
-    bullets.appendChild(bulletElement)
-  })
-  slideElement.appendChild(bullets)
+    const bullets = document.createElement('ul')
+    slide.bullets.forEach(bullet => {
+        const bulletElement = document.createElement('li')
+        bulletElement.className = CLASSNAMES.bullet
+        bulletElement.innerHTML = bullet
+        bullets.appendChild(bulletElement)
+    })
+    slideElement.appendChild(bullets)
 
-  return slideElement
+    return slideElement
 }
+
+var slide = createSlide(SLIDE)
